d16bb5dbf72424986940027010d1b4a5f4230e91
Serkan Şenkal <serkansenkal97@gmail.com>
2021-02-24 10:12:12 +0300

    Initial commit

---
+++ Assets/Scripts/Units/UnitFiring.cs
@@ -0,0 +1,51 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFiring : NetworkBehaviour
+{
+    [SerializeField] private Targeter targeter = null;
+    [SerializeField] private GameObject projectilePrefab = null;
+    [SerializeField] private Transform projectileSpawnPoint = null;
+    [SerializeField] private float fireRange = 5f;
+    [SerializeField] private float fireRate = 2f;
+    [SerializeField] private float rotationSpeed = 20f;
+
+    private float lastFireTime;
+
+    [ServerCallback]
+    private void Update()
+    {
+        Targetable targetable = targeter.GetTarget();
+
+        if (targetable == null ) { return; }
+
+        if (!CanFireAtTarget(targetable)) { return; }
+
+        Quaternion targetRotation = Quaternion.LookRotation(
+            targetable.transform.position - transform.position);
+
+        transform.rotation = Quaternion.RotateTowards(
+            transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (Time.time > (1 / fireRate) + lastFireTime)
+        {
+            Quaternion projectileRotation = Quaternion.LookRotation(
+                targetable.GetAimAtPoint().position - projectileSpawnPoint.transform.position);
+
+            GameObject projectileInstance = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileRotation);
+
+            NetworkServer.Spawn(projectileInstance, connectionToClient);
+
+            lastFireTime = Time.time;
+        }
+    }
+
+
+    private bool CanFireAtTarget(Targetable targetable)
+    {
+        return (targetable.transform.position - transform.position).sqrMagnitude
+            <= fireRange * fireRange;
+    }
+}
